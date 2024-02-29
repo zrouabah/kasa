98c3f2d34ebd36d5286a12e84cdd7403c139ae81
Rouabah <rouabah.zineb@hotmail.fr>
2024-02-29 15:27:02 +0100

    modif logo footer

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../Layout/Footer.scss';
+import logoFooter from "../assets/logo-black.png";
 
 function Footer() {
   return (
     <div className='footer'>
-      <img src="logo black.png" alt="logo" />
+      <img src={logoFooter} alt="logo" />
       <div className='footer_text'>
         <p>© 2020 Kasa. </p> 
         <p> All rights reserved</p>
